2417753a4a8175d8c937a9ff72163969a06417cf
Shaq-Mg <shaqmcg0@gmail.com>
2024-12-18 14:18:19 +0000

    SearchBarView faeture added with animation when view appears 😎

---
+++ Core/SearchBarView.swift
@@ -0,0 +1,18 @@
+//
+//  SearchBarView.swift
+//  AmurFalcon
+//
+//  Created by Shaquille McGregor on 18/12/2024.
+//
+
+import SwiftUI
+
+struct SearchBarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchBarView()
+}
---
+++ Subviews/Headers/HomeHeaderView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeHeaderView.swift
+//  AmurFalcon
+//
+//  Created by Shaquille McGregor on 18/12/2024.
+//
+
+import SwiftUI
+
+struct HomeHeaderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeHeaderView()
+}
