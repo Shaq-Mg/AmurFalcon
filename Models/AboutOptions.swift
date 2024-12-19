14d32b07ac9d08820ee00536e0c983ae8fc72599
Shaq-Mg <shaqmcg0@gmail.com>
2024-12-19 20:04:03 +0000

    UI UPDATE: Account + About view with data enums to display options 😎

---
+++ Models/AboutOptions.swift
@@ -0,0 +1,8 @@
+//
+//  AboutOptions.swift
+//  AmurFalcon
+//
+//  Created by Shaquille McGregor on 19/12/2024.
+//
+
+import Foundation
---
+++ Models/AccountOptions.swift
@@ -0,0 +1,8 @@
+//
+//  AccountOptions.swift
+//  AmurFalcon
+//
+//  Created by Shaquille McGregor on 19/12/2024.
+//
+
+import Foundation
