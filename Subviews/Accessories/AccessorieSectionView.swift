6940201f49b3622b5b2d6f903f367c8bb6e53b21
Shaq-Mg <shaqmcg0@gmail.com>
2024-12-21 21:47:01 +0000

    AccessoriesView UI with scrolling animation & changes to images

---
+++ Subviews/Accessories/AccessorieSectionView.swift
@@ -0,0 +1,18 @@
+//
+//  AccessorieSectionView.swift
+//  AmurFalcon
+//
+//  Created by Shaquille McGregor on 21/12/2024.
+//
+
+import SwiftUI
+
+struct AccessorieSectionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AccessorieSectionView()
+}
---
+++ Subviews/Accessories/AccessoriesOptionList.swift
@@ -0,0 +1,18 @@
+//
+//  AccessoriesOptionList.swift
+//  AmurFalcon
+//
+//  Created by Shaquille McGregor on 21/12/2024.
+//
+
+import SwiftUI
+
+struct AccessoriesOptionList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AccessoriesOptionList()
+}
