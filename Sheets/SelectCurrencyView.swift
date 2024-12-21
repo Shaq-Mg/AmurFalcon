61d4c8fb9f62e0c6446ab66f767559caee8d83a4
Shaq-Mg <shaqmcg0@gmail.com>
2024-12-21 15:34:03 +0000

    Select currency view + displaying it with a ZStack

--- Sheets/SelectCurrencyView.swift
+++ Sheets/SelectCurrencyView.swift
@@ -1,18 +1,18 @@
 //
-//  AboutView.swift
+//  SelectCurrencyView.swift
 //  AmurFalcon
 //
-//  Created by Shaquille McGregor on 19/12/2024.
+//  Created by Shaquille McGregor on 21/12/2024.
 //
 
 import SwiftUI
 
-struct AboutView: View {
+struct SelectCurrencyView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    AboutView()
+    SelectCurrencyView()
 }
